8e8651e8e6a64cafb97ae7b5349e31093dbe19f2
francyscastillo <francys.castillo@vault-it.com.ar>
2020-04-30 12:14:28 -0300

    cambio de prpoerties a variables de entorno, logs en el main

--- src/main/java/com/aguillen/supermarketshopping/SupermarketShoppingApplication.java
+++ src/main/java/com/aguillen/supermarketshopping/SupermarketShoppingApplication.java
@@ -1,13 +1,18 @@
 package com.aguillen.supermarketshopping;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SupermarketShoppingApplication {
 
+	final static Logger LOG = LoggerFactory.getLogger(SupermarketShoppingApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(SupermarketShoppingApplication.class, args);
+		LOG.info( "The Supermarket shopping is now running" );
 	}
 	
 }
